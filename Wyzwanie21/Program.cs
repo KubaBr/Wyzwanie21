f7699feaf49ed7132f3fdff67866e3960d51999d
Kuba Brózda <kuba.brozda@gmail.com>
2023-02-20 15:52:05 +0100

    update

--- Wyzwanie21/Program.cs
+++ Wyzwanie21/Program.cs
@@ -22,22 +22,18 @@ Console.Write($"Avarege: {statistics.Avarage:N2} ");
 Console.Write($"Min: {statistics.Min} ");
 Console.Write($"Max: {statistics.Max} ");
 Console.WriteLine();
-Console.WriteLine();
 
 Console.Write($"Avarege For: {statistics1.Avarage:N2} ");
 Console.Write($"Min For: {statistics1.Min} ");
 Console.Write($"Max For: {statistics1.Max} ");
-Console.WriteLine();
 Console.WriteLine();
 
 Console.Write($"Avarege doWhile: {statistics2.Avarage:N2} ");
 Console.Write($"Min doWhile: {statistics2.Min} ");
 Console.Write($"Max doWhile: {statistics2.Max} ");
 Console.WriteLine();
-Console.WriteLine();
 
 Console.Write($"Avarege While: {statistics3.Avarage:N2} ");
 Console.Write($"Min While: {statistics3.Min} ");
 Console.Write($"Max While: {statistics3.Max} ");
-Console.WriteLine();
 Console.WriteLine();
